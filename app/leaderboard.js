0627b099f241665571cb191ddda24294bd3498e6
reedv <villanuevareed@gmail.com>
2016-10-11 11:44:27 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -2,6 +2,26 @@
  * Created by reedvilanueva on 10/11/16.
  */
 
-console.log("Hello world");
-
 PlayersList = new Mongo.Collection('players');
+
+
+
+if(Meteor.isClient){
+  console.log("Hello client");
+
+  // This helpers keyword is a special keyword that allows us to define multiple
+  //   helper functions inside a single block of code.
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+
+  });
+
+}
+
+
+if(Meteor.isServer){
+  console.log("Hello server");
+}
+
